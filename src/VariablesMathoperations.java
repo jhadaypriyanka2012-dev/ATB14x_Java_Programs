174ccaaeae535d30b8616a88768a7652a730d232
91832 <jhadaypriyanka.2012@gmail.com>
2025-09-17 20:04:40 +0530

    VariablesMathoperations

---
+++ src/VariablesMathoperations.java
@@ -0,0 +1,9 @@
+import javax.sound.midi.Soundbank;
+
+public class VariablesMathoperations {
+    public static void main(String[] args) {
+        System.out.println('S');
+        System.out.println("Parnika");
+        System.out.println((int)5/2);
+    }
+}
